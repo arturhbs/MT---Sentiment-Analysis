d34272980fc2a76b3be2fd7d485c4ae5b1bc3f70
arturhbs <arturhenrique71@gmail.com>
2019-06-28 21:41:45 -0300

    inserting Lower case

--- read-files.py
+++ read-files.py
@@ -59,7 +59,30 @@ data_split_train_pos = []
 # tokenize_words(sentences_train_neg,data_split_train_neg)
 tokenize_words(sentences_train_pos,data_split_train_pos)
 
+# print(data_split_train_pos)
+# input()
 print("Finish tokenize\n")
+
+###### LOWERCASE WORDS
+
+def lowercase(arr, arr_lower):
+    for i in range(len(arr)):
+        for w in arr[i]:
+            arr_lower.append(w.lower())
+
+# All the new array's name
+lowercase_test_neg = []        
+lowercase_test_pos = []        
+lowercase_train_neg = []        
+lowercase_train_pos = []        
+
+# lowercase(data_split_test_neg,lowercase_test_neg)
+# lowercase(data_split_test_pos,lowercase_test_pos)
+# lowercase(data_split_train_neg,lowercase_train_neg)
+lowercase(data_split_train_pos,lowercase_train_pos)
+# print(lowercase_train_pos)
+
+print("Finish Lowercase\n")
 
 ###### STOP-WORDS
 
@@ -67,51 +90,52 @@ from nltk.corpus import stopwords
 
 # Method that need two params, first the array that has been tokenized and the second the new array that will store all the words that are different from the stopWords
 def stop_words(arr, arr_filteredWords):
-    for i in range(len(arr)):
-        for w  in arr[i] :
-            if not w in stopWords:
-                arr_filteredWords.append(w)
+    for w  in arr :
+        if not w in stopWords:
+            arr_filteredWords.append(w)
 
 stopWords = stopwords.words('english')
 stopWords.extend(['-', ')', '(', 'The', 'This', '...', '!', '--', 'A','.','?',',', 'I', '/', 'br', '<', '>', 'a', '&', ':', "'s", "''", "'re", "'ve","``"])
 
 
-# All the new arrays name
+# All the new array's name
 filtered_word_test_neg = []
 filtered_word_test_pos = []
 filtered_word_train_neg = []
 filtered_word_train_pos = []
 
 # Calling method to all new arrays
-stop_words(data_split_test_neg, filtered_word_test_neg)
-stop_words(data_split_test_pos, filtered_word_test_pos)
-stop_words(data_split_train_neg, filtered_word_train_neg)
-stop_words(data_split_train_pos, filtered_word_train_pos)
+# stop_words(lowercase_test_neg, filtered_word_test_neg)
+# stop_words(lowercase_test_pos, filtered_word_test_pos)
+# stop_words(lowercase_train_neg, filtered_word_train_neg)
+stop_words(lowercase_train_pos, filtered_word_train_pos)
 
 print("Finish stop word\n")
-
 
 ###### LEMMATIZE WORDS
 
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
+# Method that recieves two params, first the one 
 def lemmatize(arr, arr_lemmatized):
     for i in range(len(arr)):
         arr_lemmatized.append(lemmatizer.lemmatize(arr[i]))
 
+# All the new array's name
 lemmatized_word_test_neg = []
 lemmatized_word_test_pos = []
 lemmatized_word_train_neg = []
 lemmatized_word_train_pos = []
 
+# Calling method to all new array
 # lemmatize(filtered_word_test_neg, lemmatized_word_test_neg)
 # lemmatize(filtered_word_test_pos, lemmatized_word_test_pos)
 # lemmatize(filtered_word_train_neg, lemmatized_word_train_neg)
 lemmatize(filtered_word_train_pos, lemmatized_word_train_pos)
 
 print("Finish lemmatizing\n")
-input()
+
 # Test to see if the arrays are recieving the correct message;
-for i in range(len(lemmatized_word_train_pos)):
-    print(lemmatized_word_train_pos)
+
+print(lemmatized_word_train_pos)
