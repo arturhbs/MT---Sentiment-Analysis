f2d9cf3f7710989fb171196400e6634f4123fc9c
arturhbs <arturhenrique71@gmail.com>
2019-06-29 20:59:07 -0300

    Change names and delete file

--- projeto2_ArturSouza_150118783.py
+++ projeto2_ArturSouza_150118783.py
@@ -27,9 +27,9 @@ sentences_train_neg = []
 sentences_train_pos = []
 
 # Inicializing all vectors
-load_files_directory(directory_test_neg,sentences_test_neg)
+# load_files_directory(directory_test_neg,sentences_test_neg)
 load_files_directory(directory_test_pos,sentences_test_pos)
-load_files_directory(directory_train_neg,sentences_train_neg)
+# load_files_directory(directory_train_neg,sentences_train_neg)
 load_files_directory(directory_train_pos,sentences_train_pos)
 
 print("Finish reading dataset\n")
@@ -55,7 +55,7 @@ data_split_train_pos = []
 
 # Calling method to all new arrays
 # tokenize_words(sentences_test_neg,data_split_test_neg)
-# tokenize_words(sentences_test_pos,data_split_test_pos)
+tokenize_words(sentences_test_pos,data_split_test_pos)
 # tokenize_words(sentences_train_neg,data_split_train_neg)
 tokenize_words(sentences_train_pos,data_split_train_pos)
 
@@ -75,12 +75,11 @@ lowercase_train_neg = []
 lowercase_train_pos = []        
 
 # lowercase(data_split_test_neg,lowercase_test_neg)
-# lowercase(data_split_test_pos,lowercase_test_pos)
+lowercase(data_split_test_pos,lowercase_test_pos)
 # lowercase(data_split_train_neg,lowercase_train_neg)
 lowercase(data_split_train_pos,lowercase_train_pos)
 
 print("Finish Lowercase\n")
-
 ###### STOP-WORDS
 
 from nltk.corpus import stopwords
@@ -103,7 +102,7 @@ filtered_word_train_pos = []
 
 # Calling method to all new arrays
 # stop_words(lowercase_test_neg, filtered_word_test_neg)
-# stop_words(lowercase_test_pos, filtered_word_test_pos)
+stop_words(lowercase_test_pos, filtered_word_test_pos)
 # stop_words(lowercase_train_neg, filtered_word_train_neg)
 stop_words(lowercase_train_pos, filtered_word_train_pos)
 
@@ -127,7 +126,7 @@ lemmatized_word_train_pos = []
 
 # Calling method to all new array
 # lemmatize(filtered_word_test_neg, lemmatized_word_test_neg)
-# lemmatize(filtered_word_test_pos, lemmatized_word_test_pos)
+lemmatize(filtered_word_test_pos, lemmatized_word_test_pos)
 # lemmatize(filtered_word_train_neg, lemmatized_word_train_neg)
 lemmatize(filtered_word_train_pos, lemmatized_word_train_pos)
 
@@ -150,7 +149,7 @@ porter_word_train_neg = []
 porter_word_train_pos = []   
 
 # porterStemmer(lemmatized_word_test_neg, porter_word_test_neg)     
-# porterStemmer(lemmatized_word_test_pos, porter_word_test_pos)     
+porterStemmer(lemmatized_word_test_pos, porter_word_test_pos)     
 # porterStemmer(lemmatized_word_train_neg, porter_word_train_neg)     
 porterStemmer(lemmatized_word_train_pos, porter_word_train_pos) 
 
@@ -165,11 +164,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 vectorizer = CountVectorizer()
 
-X = vectorizer.fit_transform(porter_word_train_pos)   
-# Name that were separeted for the train, look that all the names are, by default, in lowercase and in alphabetical order
-
+X_pos = vectorizer.fit_transform(porter_word_train_pos)   
 terms = vectorizer.get_feature_names()
-freqs = X.sum(axis=0).A1
+freqs = X_pos.sum(axis=0).A1
 result = dict(zip(terms, freqs))
 print(sorted(result.items(), key = 
              lambda kv:(kv[1], kv[0]), reverse=True ))
+
+X_pos_test = vectorizer.fit_transform(porter_word_test_pos)
+terms = vectorizer.get_feature_names()
+freqs = X_pos_test.sum(axis=0).A1
+result = dict(zip(terms, freqs))
+print(sorted(result.items(), key = 
+             lambda kv:(kv[1], kv[0]), reverse=True ))
+# Name that were separeted for the train, look that all the names are, by default, in lowercase and in alphabetical order
+
+target = [1 if i < 12500 else 0 for i in range(25000)]
